d403caad05ead283fc243a05985a12940dc3ca08
Igor <igor.koska@gmail.com>
2018-05-16 11:47:18 +0200

    Add GET/users/me and authentication

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Desk} = require('./models/desk');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 // setup for Heroku
@@ -125,6 +126,11 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+// authentication of the user
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
